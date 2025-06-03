56e6bb6e4636bd97745e44c9e82119a943ddb8c9
keminar <linuxphp@126.com>
2025-06-03 23:24:14 +0800

    first commit

---
+++ EasyPuTTY/EasyPuTTY.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "resource.h"
+
+#define IDC_TABCONTROL 100
+
+HWND hTabCtrl;
+HWND hToolbar;
+HWND g_mainWindowHandle;
+
+// will use this structure to group fields which describe tab header and editor
+struct TabEditorsInfo {
+	int tabWindowIdentifier;
+	int tabIncrementor;
+	HWND parentWinHandle;
+	HWND tabCtrlWinHandle;
+	HMENU tabMenuHandle;
+	LOGFONT editorFontProperties;
+	HFONT editorFontHandle;
+};
+
+// Data associated with each tab control item. We will use it instead of TCITEM. First member must be TCITEMHEADER, other members we can freely define
+typedef struct tagTCCUSTOMITEM {
+	TCITEMHEADER tcitemheader;
+	HWND editorWindowHandle;
+	HWND puttyWindowHandle;
+	wchar_t* fileName;
+} TCCUSTOMITEM;
+
+// single global instance of TabEditorsInfo
+struct TabEditorsInfo g_tabEditorsInfo;
+
+void CreateToolBarTabControl(struct TabEditorsInfo *tabEditorsInfo, HWND parentWinHandle);
+void AddNewTab(HWND hTab);
+void RemoveTab(HWND hTab, int currentTab);
+LRESULT processTabNotification(HWND tabCtrlWinHandle, HMENU tabMenuHandle, HWND menuCommandProcessorWindowHandle, int code);
