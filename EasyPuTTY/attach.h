599b84ec36f60544275529c418397f72292aac70
keminar <linuxphp@126.com>
2025-06-12 16:16:06 +0800

    解决CMD attach detach显示问题

--- EasyPuTTY/attach.h
+++ EasyPuTTY/attach.h
@@ -5,3 +5,4 @@
 BOOL CALLBACK EnumPuTTYWindows(HWND hwnd, LPARAM lParam);
 HWND FindPuttyWindow();
 HWND createPuttyWindow(HINSTANCE hInstance, HWND hostWindow, const wchar_t* puttyPath);
+BOOL IsConsoleWindow(HWND hWnd);
