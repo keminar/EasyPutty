cac3a8d3cba949657b01fa188157c7b4f27cf1dd
keminar <linuxphp@126.com>
2025-06-16 00:22:38 +0800

    菜单按钮

--- EasyPuTTY/Resource.h
+++ EasyPuTTY/Resource.h
@@ -15,16 +15,24 @@
 #define IDR_MAIN_TOOLBAR                129
 #define IDD_DIALOG1                     131
 #define IDD_ATTACH                      132
+#define IDD_CREDENTIAL                  132
 #define IDD_ENUMWIN                     133
 #define IDD_SESSION                     134
+#define IDD_SETTING                     135
 #define IDC_BUTTON1                     1004
 #define IDC_REFRESH                     1004
 #define IDC_SYSLINK1                    1005
+#define IDC_BUTTON2                     1005
 #define IDC_SYSLINK2                    1006
+#define IDC_BUTTON3                     1006
 #define IDC_SESSION_NAME                1007
+#define IDC_EDIT1                       1007
 #define IDC_SESSION_IP                  1008
+#define IDC_EDIT2                       1008
 #define IDC_SESSION_PORT                1009
+#define IDC_EDIT3                       1009
 #define IDC_SESSION_CONNECT             1010
+#define IDC_BUTTON4                     1010
 #define IDC_SESSION_CREDENTIAL          1011
 #define IDC_CREDENTIAL_ADD              1012
 #define IDC_EDIT4                       1013
@@ -49,7 +57,7 @@
 #define ID_TODO                         32787
 #define IDM_SETTING                     32788
 #define IDM_SESSION                     32789
-#define IDM_CREDENTIAL                  32790 
+#define IDM_CREDENTIAL                  32790
 #define IDM_PAGEANT                     32791
 #define IDM_PUTTYGEN                    32792
 #define IDC_STATIC                      -1
@@ -59,9 +67,9 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        132
+#define _APS_NEXT_RESOURCE_VALUE        136
 #define _APS_NEXT_COMMAND_VALUE         32788
-#define _APS_NEXT_CONTROL_VALUE         1007
+#define _APS_NEXT_CONTROL_VALUE         1010
 #define _APS_NEXT_SYMED_VALUE           110
 #endif
 #endif
